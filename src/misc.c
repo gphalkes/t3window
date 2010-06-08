39ce6047acbe3f57ffcb9c6b8139eb6c0b8c654a
G.P. Halkes <hg@ghalkes.nl>
2010-06-08 10:36:53 +0200

    Changed first letter of error messages to lower case

--- src/misc.c
+++ src/misc.c
@@ -29,34 +29,34 @@ long t3_window_get_version(void) {
 const char *t3_window_strerror(int error) {
 	switch (error) {
 		case T3_ERR_SUCCESS:
-			return _("Success");
+			return _("success");
 		case T3_ERR_ERRNO:
 			return strerror(errno);
 		case T3_ERR_EOF:
-			return _("End of file");
+			return _("end of file");
 		default: /* FALLTHROUGH */
 		case T3_ERR_UNKNOWN:
-			return _("Unknown error");
+			return _("unknown error");
 		case T3_ERR_BAD_ARG:
-			return _("Bad argument passed to function");
+			return _("bad argument passed to function");
 		case T3_ERR_OUT_OF_MEMORY:
-			return _("Out of memory");
+			return _("out of memory");
 		case T3_ERR_TERMINFODB_NOT_FOUND:
-			return _("No information found in terminfo database for terminal");
+			return _("no information found in terminfo database for terminal");
 		case T3_ERR_HARDCOPY_TERMINAL:
-			return _("Terminal is a hard-copy terminal");
+			return _("terminal is a hard-copy terminal");
 		case T3_ERR_TERMINAL_TOO_LIMITED:
-			return _("Terminal provides too limited functionality");
+			return _("terminal provides too limited functionality");
 		case T3_ERR_NO_TERM:
-			return _("No terminal given and TERM environment variable not set");
+			return _("no terminal given and TERM environment variable not set");
 
 		case T3_ERR_NOT_A_TTY:
-			return _("In/output device is not a terminal");
+			return _("in/output device is not a terminal");
 		case T3_ERR_TIMEOUT:
-			return _("Timeout");
+			return _("timeout");
 		case T3_ERR_NO_SIZE_INFO:
-			return _("Size information for terminal could not be found");
+			return _("size information for terminal could not be found");
 		case T3_ERR_NONPRINT:
-			return _("Non-printable character passed for display");
+			return _("non-printable character passed for display");
 	}
 }
