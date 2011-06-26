dd859102baa6275dd9ec6a7dd42c1bc585e33861
Gertjan Halkes <hg@ghalkes.nl>
2011-06-26 14:05:56 +0200

    Mark terminal_init.c for parsing by doxygen

--- src/terminal_init.c
+++ src/terminal_init.c
@@ -11,6 +11,8 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+/** @file */
+
 #include <string.h>
 #include <termios.h>
 #include <stdlib.h>
