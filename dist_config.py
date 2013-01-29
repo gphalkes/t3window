d9eddd6674bcbaf23b321e55001df6fe887239e5
Gertjan Halkes <git@ghalkes.nl>
2013-01-29 22:52:03 +0100

    Converted .hgignore to .gitignore; converted to new python based tar building

---
+++ dist_config.py
@@ -0,0 +1,42 @@
+import os
+
+package = 'libt3window'
+excludesrc = '/(Makefile|TODO.*|SciTE.*|run\.sh|test\.c)$'
+auxsources= [ 'src/window_api.h', 'src/window_errors.h', 'src/window_shared.c' ]
+extrabuilddirs = [ 'doc' ]
+auxfiles = [ 'doc/API' ]
+
+versioninfo = '0:0:0'
+
+
+def get_replacements(mkdist):
+	return [
+		{
+			'tag': '<VERSION>',
+			'replacement': mkdist.version
+		},
+		{
+			'tag': '^#define T3_WINDOW_VERSION .*',
+			'replacement': '#define T3_WINDOW_VERSION ' + mkdist.get_version_bin(),
+			'files': [ 'src/terminal.h' ],
+			'regex': True
+		},
+		{
+			'tag': '<OBJECTS>',
+			'replacement': " ".join(mkdist.sources_to_objects(mkdist.sources, '\.c$', '.lo')),
+			'files': [ 'Makefile.in' ]
+		},
+		{
+			'tag': '<VERSIONINFO>',
+			'replacement': versioninfo,
+			'files': [ 'Makefile.in' ]
+		},
+		{
+			'tag': '<LIBVERSION>',
+			'replacement': versioninfo.split(':', 2)[0],
+			'files': [ 'Makefile.in' ]
+		}
+	]
+
+def finalize(mkdist):
+	os.symlink('.', os.path.join(mkdist.topdir, 't3window'))
--- dist_config.sh
+++
@@ -1,4 +0,0 @@
-PACKAGE=libt3window
-EXCLUDESRC="/(Makefile|TODO.*|SciTE.*|run\.sh|test\.c)$"
-GENSOURCES="src/window_api.h src/window_errors.h src/window_shared.c"
-VERSIONINFO="0:0:0"
